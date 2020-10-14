1021897f9929a5d7267a25c73a17775328d049ae
Siarhei Kazlouski <aspirin12@yandex.ru>
2020-10-14 16:07:44 +0300

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    arr = n.toString().split('');
+
+    resArr = []
+    
+    while (arr.length > 0) {
+      resArr.push(arr.pop());
+    }
+    
+    return resArr.join('').replace(/^0|-$/, '');
 }
